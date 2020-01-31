0f0772806ad1d759944d54603b915f3f4efb3dec
Phil <pm993@hotmail.com>
2020-01-31 12:22:31 -0500

    Basic commit of LAPA script

---
+++ LAPA.m
@@ -0,0 +1,17 @@
+%PA 4 - Laplace Equation by Iteration
+%ELEC 4700
+%Philippe Masson
+
+
+xSize = 10; %size of the matrix
+ySize = 10; %size of the matrix
+maxIterations = 100; %maximum number of iterations
+vMatrix = zeros(xSize,ySize); %create our matrix of values
+oldMatrix = zeros(xSize,ySize); %another matrix to be used to clone the new one
+
+for i = 1:maxIterations
+    
+    
+    
+end
+
